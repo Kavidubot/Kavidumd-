0b63104e396e36f1e0985f4a231dcb834773ed78
Kavidubot <botkavidu3@gmail.com>
2025-03-30 19:20:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG:process.evn.ALIVE_IMG || "https://avatars.githubsercontent.com/u/106251140?v=4",
+ALIVE_MSG:process.even.ALIVE_MSG || "Hello,I am kavidumd I am alive now!",
 };
